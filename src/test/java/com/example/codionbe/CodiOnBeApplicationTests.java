5e6c44ce142bc15e46d34d1a743aea46fe879b41
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-03-13 13:27:03 +0900

    Feat: initial setting

---
+++ src/test/java/com/example/codionbe/CodiOnBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codionbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodiOnBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
