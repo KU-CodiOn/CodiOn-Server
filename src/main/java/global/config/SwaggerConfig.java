90bb7c6cd0d4170f61cae030bfe64435bf2db065
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-04-15 18:03:13 +0900

    feat: swagger 기능 추가

---
+++ src/main/java/global/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package global.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("CodiOn API")
+                        .description("AI 기반 코디 추천 프로젝트 API 명세서")
+                        .version("v1.0.0"));
+    }
+}
