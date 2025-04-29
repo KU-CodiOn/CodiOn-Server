7f843cd81d176e3ba28ef9c95ccfe38a97a1e730
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-04-30 07:19:57 +0900

    feat: 토큰 재발급 API 구현

---
+++ src/main/java/com/example/codionbe/domain/member/repository/RefreshTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.codionbe.domain.member.repository;
+
+import com.example.codionbe.domain.member.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByUserId(Long userId);
+}
