19c60ef2017f02c7a22f14a86481665180131c05
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-05 05:50:38 +0900

    fix: env 수정 4

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+mkdir -p /home/ubuntu/app  # 경로가 없을 경우 자동 생성
+
 cat <<EOF > /home/ubuntu/app/.env
 export JWT_SECRET=$JWT_SECRET
 export DB_URL=$DB_URL
@@ -8,3 +10,5 @@ export DB_PASSWORD=$DB_PASSWORD
 export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 EOF
+
+echo ".env 파일 생성 완료"
