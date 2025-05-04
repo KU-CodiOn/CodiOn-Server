f4592c6d059d65fbf354fbf186adde59abc4aee1
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-05 05:43:36 +0900

    fix: env 수정 2

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-echo "✅ Generating .env file with environment variables..."
-
-# .env 파일을 새로 생성하면서 덮어씀
-cat <<EOF > /home/ubuntu/app/.env
-export JWT_SECRET=${JWT_SECRET}
-export DB_URL=${DB_URL}
-export DB_USERNAME=${DB_USERNAME}
-export DB_PASSWORD=${DB_PASSWORD}
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY}
-EOF
-
-echo "✅ .env file created at /home/ubuntu/app/.env"
+echo "export JWT_SECRET=$JWT_SECRET" > /home/ubuntu/app/.env
+echo "export DB_URL=$DB_URL" >> /home/ubuntu/app/.env
+echo "export DB_USERNAME=$DB_USERNAME" >> /home/ubuntu/app/.env
+echo "export DB_PASSWORD=$DB_PASSWORD" >> /home/ubuntu/app/.env
+echo "export AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" >> /home/ubuntu/app/.env
+echo "export AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" >> /home/ubuntu/app/.env
