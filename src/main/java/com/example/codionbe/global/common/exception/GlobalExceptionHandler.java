060ef92890e0a7e6e1875a37473ac86219b57208
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-07 03:16:44 +0900

    refactor: 폴더 구조 개편에 따른 파일 이동

--- src/main/java/com/example/codionbe/global/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/codionbe/global/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.codionbe.global.common.exception;
 
-import com.example.codionbe.global.common.CustomException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
