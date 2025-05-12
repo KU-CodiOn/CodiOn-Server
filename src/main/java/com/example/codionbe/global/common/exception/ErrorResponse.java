d06697df04596da5ef448d02f9a8626ab4c25ca8
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-12 18:55:30 +0900

    feat: 응답 dto에 Swagger 예시 및 응답 포맷 추가

--- src/main/java/com/example/codionbe/global/common/exception/ErrorResponse.java
+++ src/main/java/com/example/codionbe/global/common/exception/ErrorResponse.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "에러 응답 포맷")
 public class ErrorResponse {
+    @Schema(description = "에러 코드", example = "AUTH_401")
     private final String code;
+    @Schema(description = "에러 메시지", example = "비밀번호가 일치하지 않습니다.")
     private final String message;
 
     public ErrorResponse(String code, String message) {
