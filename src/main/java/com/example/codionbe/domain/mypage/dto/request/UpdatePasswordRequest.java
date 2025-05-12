dc1802e584c8364f59d4a315b68e139d61cef375
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-12 19:17:11 +0900

    Feat: 응답 dto에 Swagger 예시 및 응답 포맷 추가

--- src/main/java/com/example/codionbe/domain/mypage/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/example/codionbe/domain/mypage/dto/request/UpdatePasswordRequest.java
@@ -1,11 +1,18 @@
 package com.example.codionbe.domain.mypage.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "비밀번호 변경 요청")
 public class UpdatePasswordRequest {
+
+    @Schema(description = "새 비밀번호", example = "newSecurePass123!")
     private String newPassword;
+
+    @Schema(description = "새 비밀번호 확인", example = "newSecurePass123!")
     private String newPasswordCheck;
 }
+
--- src/main/java/com/example/codionbe/domain/mypage/dto/request/UpdateProfileRequest.java
+++ src/main/java/com/example/codionbe/domain/mypage/dto/request/UpdateProfileRequest.java
@@ -1,11 +1,17 @@
 package com.example.codionbe.domain.mypage.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@Schema(description = "프로필 수정 요청")
 public class UpdateProfileRequest {
+
+    @Schema(description = "변경할 닉네임", example = "변경된닉네임")
     private String nickname;
+
+    @Schema(description = "변경할 퍼스널컬러", example = "AUTUMN")
     private String personalColor;
 }
