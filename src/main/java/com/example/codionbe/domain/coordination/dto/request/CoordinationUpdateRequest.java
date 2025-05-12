4a12346ffe17c3947faaa980ea9df7b697e7c1b8
Jeongdonghun <arrrr180@konkuk.ac.kr>
2025-05-13 01:47:41 +0900

    feat: 요청 및 응답 dto에 Swagger 예시 및 응답 포맷 추가

--- src/main/java/com/example/codionbe/domain/coordination/dto/request/CoordinationUpdateRequest.java
+++ src/main/java/com/example/codionbe/domain/coordination/dto/request/CoordinationUpdateRequest.java
@@ -1,12 +1,20 @@
 package com.example.codionbe.domain.coordination.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@Schema(description = "코디 수정 요청")
 public class CoordinationUpdateRequest {
+
+    @Schema(description = "코디 날짜", example = "2024-05-12")
     private LocalDate date;
+
+    @Schema(description = "수정할 옷 ID 목록", example = "[1, 4, 5]")
     private List<Long> clothesIds;
 }
--- src/main/java/com/example/codionbe/domain/coordination/dto/request/CreateCoordinationRequest.java
+++ src/main/java/com/example/codionbe/domain/coordination/dto/request/CreateCoordinationRequest.java
@@ -1,12 +1,21 @@
 package com.example.codionbe.domain.coordination.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.List;
 
-@Data
+@Getter
+@NoArgsConstructor
+@Schema(description = "코디 등록 요청")
 public class CreateCoordinationRequest {
+
+    @Schema(description = "코디 날짜", example = "2025-05-12")
     private LocalDate date;
+
+    @Schema(description = "코디에 포함된 옷 ID 목록", example = "[1, 2, 3]")
     private List<Long> clothesIds;
 }
